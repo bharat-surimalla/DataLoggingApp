f5b06ef4d4381a9bcb3e285b523cd3601d31c0e0
bharat-surimalla <surimallabharat@gmail.com>
2019-03-02 15:44:07 -0600

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {DuartionService} from './duartion.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  jsonData = {};
+  eventData = {};
+  logData = {};
+  settings = {};
+  durationByData :number;
+  durationByEvents:number;
+  constructor(private http: HttpClient,
+              private duration : DuartionService
+    ){
+
+  }
+  ngOnInit(){
+    this.http.get('./assets/tripdata.json').subscribe((res:any)=>{
+        this.jsonData = res;
+        this.eventData = res.tripUploadEvents;   
+        this.logData = res.tripUploadData;
+        this.settings = res.tripSettings;
+        this.durationByEvents = this.duration.durationBasedOnEvents(this.eventData, this.jsonData); 
+        this.durationByData =this.duration.durationBasedOnData(this.logData, this.settings,this.jsonData);
+    
+    });
+  }
+}
+
+
+
+
---
+++ src/app/duartion.service.ts
@@ -0,0 +1,143 @@
+import { Injectable } from '@angular/core'
+
+@Injectable()
+export class DuartionService {
+
+  outBoundTimeStamps= [];
+  eventTimeStamps=[];
+  totalDurationByData : number =0;;
+  constructor() { }
+
+   durationBasedOnEvents(eventData:any, jsonData:any){
+
+      var durationByEvent =0;
+      var endTime = jsonData.endTime;
+
+      for(let i=0;i<eventData.length;i++){
+        
+          if(eventData[i].eventType ==8){
+            let x1 = new Date(eventData[i].timestamp).getTime();
+            let x2 :number;
+            let flag = 0;
+            for(let j=i+1; j< eventData.length;j++){
+              if(eventData[j].eventType == 6)
+              {
+                 x2 = new Date(eventData[j].timestamp).getTime();
+                 flag=1;
+                 break;
+              }    
+            }
+            if(flag==0){
+              x2= new Date(endTime).getTime();
+            }
+            this.eventTimeStamps.push({stamp:eventData[i].timestamp,duration:(x1-x2)/1000});
+            durationByEvent = durationByEvent + ((x1-x2)/1000);
+          }
+          if(eventData[i].eventType ==7){
+            let x1 = new Date(eventData[i].timestamp).getTime();
+            let x2 :number;
+            let flag = 0;
+            for(let j=i+1; j< eventData.length;j++){
+              if(eventData[j].eventType == 9)
+              {
+                 x2 = new Date(eventData[j].timestamp).getTime();
+                 flag =1;
+                 break;
+              }
+            }
+            if(flag==0){
+              x2= new Date(endTime).getTime();
+            }   
+            this.eventTimeStamps.push({stamp:eventData[i].timestamp,duration:(x1-x2)/1000});
+            durationByEvent = durationByEvent + ((x1-x2)/1000);
+          }
+      }
+      console.log(durationByEvent);
+      return durationByEvent;
+  }
+
+   durationBasedOnData(logData:any, settings:any, jsonData:any){
+
+     let hum_max: number;
+     let hum_min : number;
+     let temp_min:number;
+     let temp_max :number;
+     let startTime = jsonData.startTime;
+     let endTime = jsonData.endTime;
+  
+     settings.forEach((element:any)=> {
+       if(element.channelName == 'sensor1')
+       {
+         hum_max = element.max;
+         hum_min = element.min;
+       }
+       else if(element.channelName == 'sensor2')
+       {
+         temp_max = element.max;
+         temp_min = element.min;
+       }   
+     });
+
+     for(let i=0; i<logData.length; i++)
+     {
+
+      if(logData[i].channel == "sensor1")
+      {
+        if(logData[i].data>hum_max || logData[i].data < hum_min )
+        {
+         let x1 = new Date(logData[i].timestamp).getTime();
+         let x2 : any;
+         if(logData[i].timestamp == startTime || logData[i].timestamp == endTime)
+         {  
+          this.outBoundTimeStamps.push({stamp:(logData[i].timestamp),duration:480000}); 
+         }
+         else
+         {
+            for(let j=i-1; j>0;j-- )
+            {
+              if(logData[j].data<temp_max || logData[j].data >temp_min)
+              {
+                 x2 = new Date(logData[j].timestamp).getTime();     
+                 this.outBoundTimeStamps.push({stamp:(logData[i].timestamp),duration:(x2-x1)}); 
+                 console.log(x2-x1);
+                 break;
+              }            
+            }
+          } 
+
+        }
+      }
+      else if (logData[i].channel == "sensor2")
+      {
+        if(logData[i].data>temp_max || logData[i].data < temp_min )
+        {
+         let x1 = new Date(logData[i].timestamp).getTime();
+         let x2 : any;
+         if(logData[i].timestamp == startTime || logData[i].timestamp == endTime)
+         {     
+          this.outBoundTimeStamps.push({stamp:(logData[i].timestamp),duration:480000}); 
+         }
+         else
+         {
+            for(let j=i-1; j>0;j-- )
+            {
+              if(logData[j].data<temp_max || logData[j].data >temp_min)
+              {
+                 x2 = new Date(logData[j].timestamp).getTime();      
+                 this.outBoundTimeStamps.push({stamp:(logData[i].timestamp),duration:(x2-x1)}); 
+                 break;
+              }            
+            }
+          }        
+         }   
+        }    
+      }
+      var uniqueItems = Array.from(new Set(this.outBoundTimeStamps));
+      uniqueItems.forEach((element)=>{
+        this.totalDurationByData = this.totalDurationByData + element.duration;
+      });
+      console.log(this.totalDurationByData);
+      return (this.totalDurationByData/1000);    
+   } 
+   
+}
